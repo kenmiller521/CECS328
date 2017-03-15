78c8c9a6adc3c6972543925144bbf2021b995f41
Ken Miller <ken.miller521@gmail.com>
2017-03-14 20:31:38 -0700

    Made Project 3

---
+++ CECS328/Project 3/Project3Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+struct Token
+{
+	string token = "abcd"; //token being stored
+	int a_count; //total occurences in A training docs.
+	int b_count; //total occurences in B training docs.
+};
+int main()
+{
+
+	return 0;
+}
