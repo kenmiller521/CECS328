1d3f9fe32053de6413104ebd036a5e5ed59d98c0
Ken Miller <ken.miller521@gmail.com>
2017-05-10 16:16:00 -0700

    Work on project 3

--- CECS328/Project 3/Project3Main.cpp
+++ CECS328/Project 3/Project3Main.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unordered_map>
+
 unsigned int hashValue(std::string str, int size);
 
 struct Token
@@ -22,12 +23,13 @@ int main()
 	std::string userStr;
 	int tableSize = 0;
 	std::string delim = " ";
+	std::string delim2 = ",";
 	std::string token;
 	size_t pos = 0;
 
 	int counter = 0;
 	//std::unordered_map<int, std::string> table;
-	std::vector< std::list< int > > myVectorOfLists;
+	std::vector< std::list< std::string > > myVectorOfLists;
 	while (menuInput != 0)
 	{
 		std::cout << "1: " << std::endl;
@@ -48,7 +50,14 @@ int main()
 		case 2:
 			std::cout << "Enter size of table: ";
 			std::cin >> tableSize;
-			std::cout << "Enter words: ";
+			for (int i = 0; i < tableSize; i++)
+			{
+				//std::list<std::string> first;
+
+				myVectorOfLists.push_back(std::list<std::string>());
+			}
+			//std::cout << "Vector list size: " << myVectorOfLists.size();
+			std::cout << "Enter words with a space between them: ";
 			std::cin.ignore(99, '\n');
 			std::getline(std::cin, userStr);
 
@@ -56,15 +65,27 @@ int main()
 			while ((pos = userStr.find(delim)) != std::string::npos)
 			{
 				token = userStr.substr(0, pos);
-				std::cout << token << " ";
-				//userStr[counter] = std::stoi(token);
-				//counter++;
+				//std::cout << "Hash value of " << token << " is " << hashValue(token, token.length()) % tableSize << std::endl;
+				myVectorOfLists[(hashValue(token, token.length()) % tableSize)].push_back(token);
 				userStr.erase(0, pos + delim.length());
 			}
-			//for (int i = 0; i < userStr.length(); i++)
-			//{
-			//	myVectorOfLists[]
-			//}
+			for (int i = 0; i < myVectorOfLists.size(); i++)
+			{
+				if (myVectorOfLists[i].size() == 0)
+					std::cout << i << ": " << "(empty)" << std::endl;
+				else
+				{
+					std::cout << i << ":";
+					for (auto j = myVectorOfLists[i].begin(); j != myVectorOfLists[i].end(); j++)
+					{
+						std::cout << *j;
+						//myVectorOfLists.
+					}
+					std::cout << std::endl;
+				}
+					
+			}
+			std::cout << std::endl;
 			break;
 		case 3:
 			break;
@@ -83,7 +104,7 @@ unsigned int hashValue(std::string str, int size)
 	unsigned int h_value = 0;
 	for (int i = 0; i < size; i++)
 	{
-		h_value = 37 * h_value + str[i];
+		h_value = 37 * h_value + (str[i] - '0');
 	}
 	return h_value;
 }
